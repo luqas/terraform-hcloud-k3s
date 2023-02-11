55ff79af8edd05297fc9fe9191293b69a89cda59
LuQaS <lukasz.kasprzak@gmail.com>
2023-02-11 17:13:26 +0100

    internal ip for nodes

--- modules/node_group/templates/init.sh
+++ modules/node_group/templates/init.sh
@@ -6,6 +6,9 @@ apt-get install -yq \
     curl \
     ntp
 
+node_internal_ip=$(ifconfig ens10 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p') 
+
+
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_URL=https://${master_ipv4}:6443 K3S_TOKEN=${k3s_token} sh -s - \
     --kubelet-arg 'cloud-provider=external' --node-ip ${node_internal_ip}
